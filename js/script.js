6d6ae3233e268b60aa337de3d76fae787588d7c4
Emily Garvey <emilyg@scripted.org>
2019-09-27 16:36:30 +0000

    input.val

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $( "document" ).ready(function() {
 
+$("button").click(function(){
+    let message = "hello";
+
+    let input = $("input").val();
+    let newMessage = input.slice(1);
+    console.log(newMessage);
+    $(".output").append(`${input}ay`);
+
+});
+
+
+
+
+
 
 
 });
