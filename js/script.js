ab8d0fd8beea5695ac91642bdc71ca4ef72f545c
Emily Garvey <emilyg@scripted.org>
2019-10-01 20:52:16 +0000

    again

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ $("#button").click(function(){
         let firstLetter = word.toLowerCase().charAt(0);
         if (firstLetter === 'a' || firstLetter === 'e' || firstLetter === 'i' || firstLetter === 'o' || firstLetter === 'u') {
             $(".output").append(`${word}ay `);
+        } else if (firstLetter === '') {
+            $(".output").append(`Input text`);
         } else {
             let newMessage = word.slice(1);
             $(".output").append(`${newMessage}${firstLetter}ay `);
