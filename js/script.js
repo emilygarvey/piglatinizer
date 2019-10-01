3c4a059e75e140247ca89169de3ea340d255547d
Emily Garvey <emilyg@scripted.org>
2019-10-01 20:22:39 +0000

    split test

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 $( "document" ).ready(function() {
 
-$("button").click(function(){
-    let message = "hello";
-
+$("#button").click(function(){
     let input = $("input").val();
+    let split = input.split(' ');
+    console.log(split);
+    $(".output").text(split)
     let newMessage = input.slice(1);
-    console.log(newMessage);
-    $(".output").append(`${input}ay`);
+    let firstLetter = input.charAt(0);
+    //$(".output").text(`${newMessage}${firstLetter}ay`);
 
 });
 
